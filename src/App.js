f3de55224eb93117a3973adb9882d0948b925f01
chino <guzhanliang1@gmail.com>
2023-01-08 23:21:01 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return 
+      // <p><header></header>
+      // <body>
+      //   <p>this is an example</p>
+      //   <li>list1</li>
+      //   <li>list2</li>
+      //   <li>list3</li>
+
+        
+      // </body></p>
+      
+
+  
 }
+
+
 
 export default App;
