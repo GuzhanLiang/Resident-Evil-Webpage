85ef284a57c88a192401b8730888376e4e830aff
chino <guzhanliang1@gmail.com>
2023-01-19 11:17:51 -0500

    slider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-
-
+import "./works.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return 
-      // <p><header></header>
-      // <body>
-      //   <p>this is an example</p>
-      //   <li>list1</li>
-      //   <li>list2</li>
-      //   <li>list3</li>
+  
 
-        
-      // </body></p>
+  return(
+    <div className="slider">
+      <div class="color-overlay d-flex justify-content-center align-items-center">
+        <div className="container">
+          <div class="arrow-left">
+            <i class="fas fa-arrow-left"></i>
+          </div>
+          <div class="arrow-right">
+            <i class="fas fa-arrow-right"></i>
+          </div> 
+        </div>
+      </div>   
+    </div>
+)
       
 
   
