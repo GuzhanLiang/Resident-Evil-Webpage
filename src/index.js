2cd0ae8726fab708a57408bb017ff4425631af14
chino <guzhanliang1@gmail.com>
2023-01-30 01:27:17 -0500

    slider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
 
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
